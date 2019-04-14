9d7ba7f65f8f241169fb88d93317e99c4baf5d95
ranahesham7 <47699113+ranahesham7@users.noreply.github.com>
2019-04-14 12:44:52 +0200

    Add files via upload

---
+++ initCentroids.m
@@ -0,0 +1,6 @@
+%random initalization of K centroids.
+function centroids = initCentroids(X, K)
+    centroids = zeros(K,size(X,2)); 
+    randidx = randperm(size(X,1));
+    centroids = X(randidx(1:K), :);
+ end
